b88675758157534f42e6f0661b7285db6d161605
Guilherme Sussel <guisussel@gmail.com>
2016-10-24 14:03:25 -0200

    Update calc.c

--- calc.c
+++ calc.c
@@ -4,42 +4,14 @@
 #define MAX 1000
 struct pilha
 {
-    link topo;      /* t é o topo da pilha -- proximo espaco vazio do vetor */
-    //int v[MAX]; /* v é o vetor que armazena os elementos da pilha */
+    int t;      /* t é o topo da pilha -- proximo espaco vazio do vetor */
+    int v[MAX]; /* v é o vetor que armazena os elementos da pilha */
 };
 
 /* Define um novo tipo de dado chamado Pilha que é um ponteiro para "struct pilha". */
 typedef struct pilha * Pilha;
 
-struct celula {
-  int item;
-  link next;
-};
-typedef struct celula * link;
-
-Pilha novaPilha () {
-  Pilha p;
-  p = malloc(sizeof(struct pilha));
-  if (p == NULL) {
-    printf ("Erro ao alocar meória para a pilha\n");
-    exit(-1);
-  }
-  p->topo = NULL;
-  return p;
-}
-
-link novoNo (int item, link next) {
-  link n = malloc(sizeof(struct celula));
-  if(n == NULL) {
-    printf ("Erro ao alocar novoNo\n");
-    exit(-1);
-  }
-  n->item = item;
-  n->next = next;
-  return n;
-}
-
-/* Aloca espaço para armazenar uma nova Pilha 
+/* Aloca espaço para armazenar uma nova Pilha */
 Pilha novaPilha () {
     Pilha p = malloc(sizeof(*p));
     if (p == NULL)
@@ -47,125 +19,84 @@ Pilha novaPilha () {
             printf("Algum erro aconteceu !\n");
             exit(-1);
         }
-    p->t = 0; //devemos inicializar o topo com 0 
+    p->t = 0; /*inicializar o topo com 0 */
     return p;
-}*/
-
+}
 /* Libera memória de uma dada pilha p */
-//void destroiPilha (Pilha p)
-//{
-//   free(p);
-//}
-void destroiPilha (Pilha p) {
-  while(!estaVazia(p)) {
-    pop(p);
-  }
-  free(p);
+void destroiPilha (Pilha p)
+{
+    free(p);
 }
-
-
-void imprimePilha(Pilha p) {
-  link t;
-  for (t = p->topo; t != NULL; t = t->next){
-    printf ("%c ", t->item);
-  }
-  printf("\n");b
-  return ;
+/*inserir novo elemento na pilha */
+void push (Pilha p, int valor) {
+    p->v[(p->t)++] = valor;
 }
-
-#if 0
-void push (Pilha p, int item) {
-  link t = novoNo(item, NULL);
-  if(p->topo == NULL) {
-    p->topo = t;
-    return;
-  }
-  t->next = p->topo;
-  p->topo = t;
-}
-#endif
-
-/* Operação de inserir novo elemento na pilha */
-//void push (Pilha p, int valor) {
-//    p->v[(p->t)++] = valor;
-//}
-void push (Pilha p, int item) {
-  p->topo = novoNo(item, p->topo);
-}
-
 /* Operação de remover um elemento da pilha */
-//int pop (Pilha p) {
-//    return p->v[--(p->t)];
-//}
 int pop (Pilha p) {
-  int aux;
-  link t;
-  if(p->topo == NULL) {
-    printf ("Erro de pilha vazia\n");
-    exit(-1);
-  }
-  aux = p->topo->item;
-  t = p->topo;
-  p->topo = t->next;
-  free (t);
-  return aux;
+    return p->v[--(p->t)];
 }
-
-/* Operação para pegar o elemento do topo da pilha */
-//int topo (Pilha p) {
-//    return p->v[p->t - 1];
-//}
-int pegaTopo(Pilha p) {
-  if (p->topo == NULL) {
-    printf("A pilha esta vazia\n");
-    return 0;
-  }
-  return p->topo->item;
+/*elemento do topo da pilha */
+int topo (Pilha p) {
+    return p->v[p->t - 1];
 }
+/*infixa para a notação posfixa */
+int infixoParaPosfixo (char * entrada, char * saida, int n)
+{
+    Pilha p = novaPilha();
+    int i,k ;
+    int j = 0;
+    char c;
+    push(p, '(');
+    for (i = 0; entrada[i] != '\0' &&  i < n; i++)
+        {
+            if((j-1 > 0) && (saida[j-1] != ' '))
+                saida[j++]  = ' ';
+            if(entrada[i] == '(') {
+                push(p, entrada[i]);
+            } else if(entrada[i] == ')'){
+                while (1) {
+                    c = pop(p);
+                    if(c == '(') break;
+                    saida[j++] = c;
+                    saida[j++] = ' ';
+                }
+            } else if((entrada[i] == '+') || (entrada[i] == '-')){
+                while (1) {
+                    c = topo(p);
+                    if(c == '(') break;
+                    pop(p);
+                    saida[j++] = c;
+                    saida[j++] = ' ';
+                }
+                push(p, entrada[i]);
+            } else if((entrada[i] == '*') || (entrada[i] == '/')) {
+                while (1) {
+                    c = topo(p);
+                    if(c == '(' || c == '+' || c == '-'  ) break;
+                    pop(p);
+                    saida[j++] = c;
+                    saida[j++] = ' ';
+                }
+                push(p, entrada[i]);
+            } else if (entrada[i] >= '0' && entrada[i] <= '9') {
+                while (entrada[i] >= '0' && entrada[i] <= '9') {
+                    saida[j++] = entrada[i];
+                    i++;
+                }
+                saida[j++] = ' ';
+                i--;
+            }
 
-/* Transforma a notação infixa para a notação posfixa */
-char * infixoParaPosfixo (char * entrada, char * saida, int t) {
-  int i; 
-  int j;
-  int aux;
-  j = 0;
-  Pilha p = novaPilha();
-  for ( i = 0; entrada[i] != '\0' && (i < 255); i++ ){
-    if (entrada[i] >= '0' && entrada[i] <= '9') {
-      saida[j++] = entrada[i];
-    } else if (entrada[i] == '+' || entrada[i] == '-') {
-        
-      while(!estaVazia(p)) {
-        aux = pegaTopo(p);
-        if (aux == '(' ) break;
-        pop(p);
-        saida[j++] = aux;
-      }
-      push(p, entrada[i]);
-
-    } else if ( entrada[i] == '*' || entrada[i] == '/') {
-      while(!estaVazia(p)) {
-        aux = pegaTopo(p);
-        if (aux == '(' || aux == '+' || aux == '-') break;
-            pop(p);
-            saida[j++] = aux;
-      }
-      push(p, entrada[i]);
-    } else if ( entrada[i] == ')' ) {
-      while(1) {
-	aux = pop(p);
-	if (aux == '(') break;
-	saida[j++] = aux;
-      }
-    } else if (entrada[i] == '(' ) {
-      push(p, entrada[i]);
+        }
+    while (1) {
+        c = pop(p);
+        if(c == '(') break;
+        saida[j++] = c;
+        saida[j++] = ' ';
     }
-  }
-  while (!estaVazia(p))
-    saida[j++] = pop(p);
-  saida[j] = '\0';
-  destroiPilha(p);
-  return saida;
+    saida[j] = '\0';
+    destroiPilha(p);
+    return 0;
 }
 
 int bemEncaixado (char* s) {
@@ -187,44 +118,50 @@ int bemEncaixado (char* s) {
             pop(p);
         }
     }
-    if(p->topo > 0)
+    if(p->t > 0)
         resultado = 0;
     destroiPilha(p);
     return resultado;
 }
 
 
-int calcula (char * s) {
-  int i;
-  int resp = 0;
-  int a, b;
-  Pilha p = novaPilha();
-  for (i = 0; s[i] != '\0'; i++) {
-    if(s[i] >= '0'  && s[i] <= '9') {
-      push(p, s[i] - '0');
-      continue;
+int calcula ( char * s ) {
+    int i = 0;
+    int d = 0,k;
+    int numero = 0;
+    Pilha p = novaPilha();
+    int resultado ;
+    int a,b;
+    while  (s[i] != '\0') {
+        if(s[i] >= '0' && s[i] <= '9' ) {
+            sscanf(&s[i], "%d", & numero);
+            while(s[i] >= '0' && s[i] <= '9')
+                i++;
+            i--;
+            push(p, numero);
+        } else if (s[i] == '+') {
+            b = pop(p);
+            a = pop(p);
+            push (p, a + b);
+        } else if (s[i] == '-') {
+            b = pop(p);
+            a = pop(p);
+            push (p, a - b);
+        } else if (s[i] == '*') {
+            b = pop(p);
+            a = pop(p);
+            push (p, a * b);
+        } else if (s[i] == '/') {
+            b = pop(p);
+            a = pop(p);
+            push (p, a/b);
+        }
+        i++;
     }
-    if (s[i] == '+') {
-      a = pop(p);
-      b = pop(p);
-      push(p, a + b);
-    } else if (s[i] == '*') {
-      a = pop(p);
-      b = pop(p);
-      push(p, a * b);
-    } else if (s[i] == '-') {
-      a = pop(p);
-      b = pop(p);
-      push (p, b - a); // para testar
-    } else if (s[i] == '/') {
-      a = pop(p);
-      b = pop(p);
-      push (p, b/a); // para testar
-    }
-  }
-  resp = pop(p);
-  destroiPilha(p);
-  return resp;
+
+    resultado = topo(p);
+    destroiPilha(p);
+    return resultado;
 }
 
 
